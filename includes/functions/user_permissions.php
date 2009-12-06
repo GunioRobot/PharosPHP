7dae323cc3b35c8fb1e0293d4c56d9e2568fa433
Matt Brewer <matt.brewer@me.com>
2009-12-06 10:38:10 -0500

    Updated redirect() for use with new routing system

--- includes/functions/user_permissions.php
+++ includes/functions/user_permissions.php
@@ -16,24 +16,11 @@
 	
 		global $db;
 	
-		list($useMobileVersion, $loadViaAjax) = use_mobile_version();
-	
-		// Page we're looking for (won't be set if it's just index.php - just show welcome.php to everyone)
-		$_GET['pid'] = get("pid", $useMobileVersion ? IPHONE_WELCOME_PID : WELCOME_PID);
-	
 		// Check that user is logging on to right domain
 		if ( session('domain_id') != DOMAIN_ID ) $failed = true;
 	
 		if ( !session("uid") ) {
-		
-			// Pass on the GET vars and log will pass on to index.php so log takes you back to where you wanted to go
-			$get_string = '';
-			foreach ( $_GET as $key => $value ) {
-				$get_string .= '&'.$key.'='.$value;
-			}
-					
-			redirect(HTTP_SERVER.ADMIN_DIR.'log.php?'.$get_string);
-
+			redirect(HTTP_SERVER.ADMIN_DIR.'session/login/');
 		}
 		
 		// If good to go
