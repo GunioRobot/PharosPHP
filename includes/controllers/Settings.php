81610cda23117703ae3aa152f972c0f37aea3381
Matt Brewer <matt.brewer@me.com>
2009-12-12 10:45:15 -0500

    Initial Commit

---
+++ includes/controllers/Settings.php
@@ -0,0 +1,246 @@
+<?php
+
+	require_once CLASSES_DIR.'TableController.php';
+
+	class Settings extends TableController {
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Called once, in index.php (to create the controller)
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function __construct() {
+			
+			parent::__construct("Setting","general_settings");
+						
+			$this->dataKey = "setting_id";			
+			$this->tableColumns();
+						
+		}
+		
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Builds the columns array to use for the Table output
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		protected function tableColumns() {
+			$this->table->columns = array();
+			$this->table->columns[] =  array('name' => 'Name', 'key' => 'setting_name', 'class' => 'center');
+			$this->table->columns[] =  array('name' => 'Date Added', 'key' => 'date_added', 'class' => 'center');
+			$this->table->columns[] =  array('name' => 'Last Updated', 'key' => 'last_updated', 'class' => 'center');
+			$this->table->columns[] =  array('name' => 'Action', 'class' => 'actions');
+		}
+		
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Builds the data array to pass to the Table class
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		protected function buildData($sql) {
+			
+			$data = array();
+			if ( $sql ) {
+							
+				for ( $info = $this->db->Execute($sql), $i = 1; !$info->EOF; $info->moveNext(), $i++ ) {
+
+					$id = $info->fields[$this->dataKey];
+
+					$class = ( $i % 2 ) ? 'listTier1' : 'listTier2';
+					$row = array('class' => $class, 'data' => array());
+
+					$row['data'][] = format_title($info->fields['setting_name']);
+					$row['data'][] = format_date($info->fields['date_added'],true);
+					$row['data'][] = format_date($info->fields['last_updated'],true);
+
+					$actions = '<a href="'.edit(__CLASS__,$id).'" title="Edit this '.$this->type.'">Edit</a>';
+
+					if ( is_super() ) {
+						$actions .= '&nbsp;&nbsp;|&nbsp;&nbsp;';
+						$actions .= '<a href="'.delete(__CLASS__,$id).'" title="Delete this '.$this->type.'">Delete</a>';
+					}
+
+
+					$row['data'][] = $actions;
+
+					$data[] = $row;
+				}
+
+			} $this->table->data = $data;
+			
+		}
+		
+		
+		//////////////////////////////////////////////////////////////////
+		// 
+		//	Default controller action
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function index() {
+			redirect(manage(__CLASS__));
+		}
+		
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Lists out the table of settings
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function manage($orderField='last_updated',$orderVal='asc',$page=1,$filter='') {
+			
+			$this->javascript('confirmDelete.php');
+												
+			$this->table->current_page = intval($page);
+
+			$where = $this->search($filter);
+			$order = $this->order($orderField,$orderVal);
+			
+			$this->table->basic_a = controller_link(__CLASS__,"/".__FUNCTION__."/");
+			$this->table->get_links = "$orderField/$orderVal/";
+
+			$sql = "SELECT COUNT(".$this->table->id.'.'.$this->dataKey.") as total FROM ".$this->table->id." ".$where.$order;
+			list($total, $page_count, $start) = $this->table->paginate($sql);
+			
+			// Build the data array to pass to the table
+			$sql = "SELECT * FROM ".$this->table->id." ".$where.$order." LIMIT ".$start.",".$this->table->rows_per_page;
+			$this->buildData($sql);
+
+			$view =  '<div class="titleTabs">
+		            <h1 id="page_title">'.$this->type.' Library</strong></h1>
+		            <form id="'.$this->table->id.'_form" action="'.$this->table->basic_a."$orderField/$orderVal/$page/".'" method="post">
+		            <div class="contentTabCap"></div><div class="contentTab"><input id="search" name="search" value="'.$this->table->search.'"/><a href="#" onClick="$('."'".'#'.$this->table->id.'_form'."'".').submit();" class="inputPress">Search</a></div>';
+
+				if ( is_super() ) {
+					$view .= '<div class="contentTabCap"></div><div class="contentTab"><a href="'.create(__CLASS__).'" title="Create New '.$this->type.'" class="tabAdd">Add</a></div>';
+				}
+
+		       $view .= '</form>
+		            <br clear="all" />
+		          </div>'.
+		          $this->table->get_html($this->table->current_page, $page_count, $start, $total);
+
+			$this->output($view);
+
+		}
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Shows the edit page
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function edit($id) {
+			
+			$this->javascript('tiny_mce_include.php');
+			
+			// Required by profile class and repost_mod
+			@define('PROFILE_TABLE', $this->table->id);
+			@define('PROFILE_TITLE', $this->type);
+			@define('PROFILE_ID', $this->dataKey);
+			@define('CURRENT_HTML_FILE', "edit_setting.html");
+
+			// Template tags to pull from database and replace
+			$fields = array(
+
+				array('name' => PROFILE_ID, 'type' => 'display'),
+				array('name' => '{TYPE}', 'type' => 'static', 'value' => PROFILE_TITLE),
+				array('name' => '{manage}', 'type' => 'static', 'value' => controller_link(__CLASS__)),
+				array('name' => '{new}', 'type' => 'static', 'value' => create(__CLASS__)),
+				array('name' => '{form_link}', 'type' => 'static', 'value' => save(__CLASS__,$id)),
+				array('name' => '{data_key}', 'type' => 'static', 'value' => PROFILE_ID),
+				
+				
+				array('name' => 'setting_name', 'type' => 'text', 'size' => '50' , 'max' => '200'),
+				array('name' => 'setting_value', 'type' => 'text_area', 'row' => '8', 'col' > '92', 'width' => '738px', 'height' => '100px'),
+				array('name' => 'setting_notes', 'type' => 'text_area', 'row' => '8', 'col' > '92', 'width' => '738px', 'height' => '100px'),
+				array('name' => 'date_added', 'type' => 'date_added'),
+				array('name' => 'last_updated', 'type' => 'last_updated'),
+
+				array('name' => 'content_type_id', 'type' => 'hidden', 'value' => SETTING_TYPE_ID),
+				array('name' => 'content_type_name', 'type' => 'hidden', 'value' => strtolower(PROFILE_TITLE))
+
+			);
+			
+
+			// Run throught the parser and spit out the page
+			require_once CLASSES_DIR.'Profile.php';
+			$profile = new Profile($fields);
+			
+			if ( $id > 0 ) $this->output($profile->display($this->dataKey, $id));
+			else $this->output($profile->display());
+						
+		}
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	For creating new entry, but just calls the edit code
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function create() {
+			$this->edit(0);
+		}
+		
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Performs save operations (whether new or old entry)
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function save($id=0) {
+
+			global $db;					
+			require_once REPOST_DIR.'profile.php';
+			redirect(edit(__CLASS__,$id));
+			
+		}
+		
+		
+		//////////////////////////////////////////////////////////////////
+		//
+		//	Deletes an entry, used in conjunction with "confirmDelete"
+		//	javascript jAlert stuff
+		//
+		//////////////////////////////////////////////////////////////////
+		
+		public function delete($id) {
+			
+			if ( ($confirmed = post("confirmed")) === "true" ) {
+
+				// Delete the note itself
+				$sql = "DELETE FROM ".$this->table->id." WHERE $this->dataKey = '$id' LIMIT 1";
+				$this->db->Execute($sql);
+				
+				$obj = array('error' => false, 'redirect' => controller_link(__CLASS__,'/manage/'));
+				echo json_encode((object)$obj);
+				exit;
+				
+			} else {
+				
+				echo "[link]".delete(__CLASS__,$id)."[/link]";
+				echo "This $this->type will be permanently removed.<br /><br />";
+				echo "<strong>This action cannot be undone.</strong><br />";
+				exit;
+				
+			}
+			
+		}
+		
+	}
+          
+
+?>
