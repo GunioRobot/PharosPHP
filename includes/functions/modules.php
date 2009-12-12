ce4d78ccd235a3d0645a7cc8bbef55c97698ac41
Matt Brewer <matt.brewer@me.com>
2009-12-12 17:39:30 -0500

    Removed load_modules() function

--- includes/functions/modules.php
+++ includes/functions/modules.php
@@ -1,34 +1,26 @@
 <?
 
-	function load_modules() {
-		$folder = MODULES_DIR;
-		if ($handle = opendir($folder)) {
-			while (false !== ($file = readdir($handle)) ) {
-				if ($file != "." && $file != ".." ) {
-					if ( is_dir($folder.$file) ) {
-						$h2 = opendir($folder.$file);
-						while ( false !== ($f2 = readdir($handle)) ) {
-							if ( $f2 != "." && $f2 != ".." && !is_dir($folder.$file.$f2) && $f2 === "include.php" ) {
-								include $folder.$file.$f2;
-							}
+	$modules = array();
+	
+	function load_module($name) {
+		
+		global $modules;
+		
+		if ( !isset($modules[$name]) ) {
+		
+			$folder = MODULES_DIR;
+			if ($handle = opendir($folder)) {
+				while (false !== ($file = readdir($handle)) ) {
+					if ($file != "." && $file != ".." && is_dir($folder.$file) && $file === $name ) {
+						if ( @file_exists($folder.$file.'/include.php') ) {
+							include $folder.$file.'/include.php';
+							$modules[$name] = $folder.$file.'/include.php';
 						}
 					}
 				}
 			}
 		}
-	}
-	
-	function load_module($name) {
-		$folder = MODULES_DIR;
-		if ($handle = opendir($folder)) {
-			while (false !== ($file = readdir($handle)) ) {
-				if ($file != "." && $file != ".." && is_dir($folder.$file) && $file === $name ) {
-					if ( @file_exists($folder.$file.'/include.php') ) {
-						include $folder.$file.'/include.php';
-					}
-				}
-			}
-		}
+		
 	}
 
 ?>
