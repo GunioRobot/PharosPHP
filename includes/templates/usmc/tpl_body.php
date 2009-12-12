ce5e269688076ca123b990d6d62867601c59a82f
Matt Brewer <matt.brewer@me.com>
2009-12-12 12:13:36 -0500

    Moved sidebar code to new file "tpl_sidebar.php" in theme folder

--- includes/templates/usmc/tpl_body.php
+++ includes/templates/usmc/tpl_body.php
@@ -36,38 +36,10 @@
 
 <? else : ?>
 
-	<?
-
-		if ( !isset($_GET['pid']) ) $_GET['pid'] = '';
-	
-		require_once CLASSES_DIR.'Sidebar.php' ;
-		$sidebar = new Sidebar();
-		$page = $sidebar->info($_GET['pid']);
-		define('CURRENT_HTML_FILE', $page['pg']);
-	
-		$myFile = explode('?',$page['pg']);
-		$myFile[0] = html_entity_decode(preg_replace('/<[^>]*>/', '', $myFile[0]));
-		$myFile[1] = html_entity_decode(preg_replace('/<[^>]*>/', '', $myFile[1]));
-		$option = explode('&',$myFile[1]);
-		for($i=0; $i<count($option); $i++){
-			$item = explode('=',$option[$i]);
-			if ( substr($item[1],0,2) == '%%' ) {
-				$_GET[$item[0]] = eval(substr($item[1],2));
-			} else $_GET[$item[0]]=$item[1];
-		}
-
-	?>
-
-		<div id="contentWrapper">
-	    <div id="lNav" align="center">
-	        <b class="btop"><b></b></b>
-	        <div id="lNavContent" align="left">
-	        	<? echo $sidebar->show($_GET['pid']); ?>
-	         </div>
-	        <b class="bbot"><b></b></b>
-	    </div> 
-	    <div id="content">
-			<?=$controller->output()?>
-	    </div>
+	<div id="contentWrapper">
+    <? require_once TEMPLATE_DIR.'tpl_sidebar.php' ?>
+    <div id="content">
+		<?=$controller->output()?>
+    </div>
 	
 <? endif ?>
