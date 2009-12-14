4c962309212f71736b9aadf396d7757b0e5c0a77
Matt Brewer <matt.brewer@me.com>
2009-12-14 16:55:29 -0500

    Updated fusion charts module

--- includes/modules/fusion_charts/functions/functions.php
+++ includes/modules/fusion_charts/functions/functions.php
@@ -3,5 +3,9 @@
 	function fusion_chart($name='') {
 		return MODULES_SERVER.'fusion_charts/swf/'.$name;
 	} 
+	
+	function include_fusion_chart_js() {
+		return '<script type="text/javascript" src="'.MODULES_SERVER.'fusion_charts/includes/fusionCharts.js"></script>';
+	}
 
 ?>
