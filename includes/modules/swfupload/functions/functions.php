3775b24c340f3ad70e4f10bfc589960dc5ee1439
Matt Brewer <matt.brewer@me.com>
2009-12-14 11:34:06 -0500

    Added swf_data_array() to help using the module's javascript

--- includes/modules/swfupload/functions/functions.php
+++ includes/modules/swfupload/functions/functions.php
@@ -11,5 +11,16 @@
 	function swf_upload_path() {
 		return MODULES_SERVER.'swfupload/swf/swfupload.swf';
 	}
+	
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	//	Just returns the array to use when calling the javascript file
+	//
+	/////////////////////////////////////////////////////////////////////////////////
+	
+	function swf_data_array($id, $key, $table, $postName, $storeFileSize=true, $storeFileType=true) {
+		return array("visible" => ($id>0), "key" => $key, "id" => $id, "table" => $table, "file_post_name" => $postName, "store_filesize" => ($storeFileSize?"true":"false"), "store_filetype" => ($storeFileType)?"true":"false");
+	}
 
 ?>
