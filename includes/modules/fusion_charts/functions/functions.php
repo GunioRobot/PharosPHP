2d8957049a88e764465623fa7535978a839e2afb
Matt Brewer <matt.brewer@me.com>
2009-12-14 16:14:57 -0500

    Added some PHP functions from the Fusion Chart library to help out

---
+++ includes/modules/fusion_charts/functions/functions.php
@@ -0,0 +1,7 @@
+<?
+
+	function fusion_cart($name='') {
+		return MODULES_SERVER.'fusion_charts/swf/'.$name;
+	} 
+
+?>
--- includes/modules/fusion_charts/include.php
+++ includes/modules/fusion_charts/include.php
@@ -1,3 +1,3 @@
 <?
-
+	require_once dirname(__FILE__).'/functions/autoload.php';
 ?>
