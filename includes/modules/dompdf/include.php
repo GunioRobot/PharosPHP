55f7222806ce695a63deeede9184234e618087ec
Matt Brewer <matt.brewer@me.com>
2009-12-14 10:40:17 -0500

    Rewrote DomPDF module

---
+++ includes/modules/dompdf/include.php
@@ -0,0 +1,6 @@
+<?
+
+	// Simple setup, load in all function files (injecting into application)
+	require_once dirname(__FILE__).'/functions/autoload.php';
+	
+?>
---
+++ includes/modules/dompdf/version.php
@@ -0,0 +1,6 @@
+<?
+
+	define('PROFILE_VERSION_MAJOR', 1);
+	define('PROFILE_VERSION_MINOR', 0);
+	
+?>
