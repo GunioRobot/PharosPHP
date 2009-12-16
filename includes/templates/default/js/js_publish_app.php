0f45c92f6642be5884f89a58c1b6ee12253c1fb8
Matt Brewer <matt.brewer@me.com>
2009-12-16 12:22:52 -0500

    Updated JS files for application publishing, renaming "publish_app" to "js_publish_app" for autoloading in system

--- includes/templates/default/js/js_publish_app.php
+++ includes/templates/default/js/js_publish_app.php
@@ -11,13 +11,24 @@
 			$.alerts.okButton = "&nbsp;Publish Application&nbsp;";
 
 			// Grab the link the publish button would have went to, and then display the message in the window
-			var link = '<?=HTTP_SERVER.ADMIN_DIR?>repost.php?pid=<?=$_GET['pid']?>&action=publish_app&app_id=<?=$CURRENT_APP_ID?>';
 			var message = "Publishing this application will make all changes since last publication active.<br /><br />";
 			message += "<strong>User applications will update when they are launched and connected to the internet.</strong><br /><br />";
 
 			jConfirm(message, "Publish this Application", function(r) {
-				if ( r ) window.location.replace(link);
+				if ( r ) {
+				
+					$.get($(e.target).attr('href'), '', function(data) {
+						
+						$.alerts.okButton = "&nbsp;Continue&nbsp;";
+						jAlert(data.message, data.title, function(r) {
+							// Do nothing, just remove the alert window
+						});
+						
+					}, "json");
+					
+				}
 			});
+			
 		});
 		
 	});
--- includes/templates/default/js/manage_applications.php
+++ includes/templates/default/js/manage_applications.php
@@ -4,7 +4,7 @@
 	$(function() {
 				
 		// Gets all the "Delete" links in the table and reconfigures to use with jConfirm
-		$('.list a:contains("Publish")').click(function(e) {
+		$('.list td.actions a:contains("Publish")').click(function(e) {
 			
 			// Stop the page from moving
 			e.preventDefault();
@@ -13,12 +13,22 @@
 			$.alerts.okButton = "&nbsp;Publish Application&nbsp;";
 			
 			// Grab the link the publish button would have went to, and then display the message in the window
-			var link = '<?=HTTP_SERVER.ADMIN_DIR?>'+$(e.target).attr('href');
 			var message = "Publishing this application will make all changes since last publication active.<br /><br />";
 			message += "<strong>User applications will update when they are launched and connected to the internet.</strong><br /><br />";
 			
 			jConfirm(message, "Publish this Application", function(r) {
-				if ( r ) window.location.replace(link);
+				if ( r ) {
+				
+					$.get($(e.target).attr('href'), '', function(data) {
+						
+						$.alerts.okButton = "&nbsp;Continue&nbsp;";
+						jAlert(data.message, data.title, function(r) {
+							// Do nothing, just remove the alert window
+						});
+						
+					}, "json");
+					
+				}
 			});
 			
 		});
