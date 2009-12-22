4257ea5789ab910165dd2d608331f3cb2ce79811
Matt Brewer <matt.brewer@me.com>
2009-12-22 13:21:07 -0500

    date_time for type now much cleaner code

--- includes/modules/profile/form_types/create/date_time.php
+++ includes/modules/profile/form_types/create/date_time.php
@@ -1,72 +1,71 @@
 <?
-$dt=explode(' ',$value);
-$current_date=$dt[0];
-$current_time=$dt[1];
-$tag=$form_array['name'];
-// ====================== (Choose date) =============================
-if($current_date == '' && isset($_GET[$tag])){
-	$current_date = $_GET[$tag];
-}
-if($current_date == '')
-{
-	$da= getdate();
-	$dd = date ("d", mktime (0,0,0,$da['mon'],$da['mday'],$da['year']));
-	$mm = date ("m", mktime (0,0,0,$da['mon'],$da['mday'],$da['year']));
-	$yy = date ("Y", mktime (0,0,0,$da['mon'],$da['mday'],$da['year']));
-}
-else
-{
-	$yy = Substr($current_date,0,4);
-	$mm = Substr($current_date,5,2);
-	$dd = Substr($current_date,8,2);
-}
-// ==================================================================
-// ========================= (Month) ================================
+	
+	$tag=$form_array['name'];
+	$date = ($value === "" || $value === "0000-00-00 00:00:00") ? new DateTime() : new DateTime($value);
+	
+//	var_dump($value,$date->format("m-d-Y \a\t H:i"));exit;
+	
+	$dd = $date->format('d');
+	$mm = $date->format('m');
+	$yy = $date->format('Y');
+
+
+	// Month Chooser
 	$item = '<select name="'.$tag.'_month">';
-		for($i=1; $i<=12; $i++){
-			$check='';if($i == $mm){$check='selected';}
-			$item .= '<OPTION value="'.$i.'" '.$check.'>'.date ("F", mktime (0,0,0,$i+1,00,0000)).'</OPTION>';
-		}
-	$item .= '</select>';
-// ==================================================================
-// ========================== (Day) =================================
+	for ( $i = 1; $i <= 12; $i++ ) {
+		$item .= '<option value="'.$i.'" '.($i==$mm?"selected":"").'>'.date ("F", mktime (0,0,0,$i+1,00,0000)).'</option>';
+	} $item .= '</select>';
+
+	
+	// Day Chooser
 	$item .= '<select name="'.$tag.'_day" style="margin-left:10px;">';
-	for($i=1; $i<=31; $i++){
-		$num=$i;if($i < 10){$num='0'.$i;}
-		$check='';if($num == $dd){$check='selected';}
-		$item .= '<OPTION value="'.$num.'" '.$check.'>'.$num.'</OPTION>';
-	}
-	$item .= '</select>';
-// ==================================================================
-// ========================== (Year) ================================
-	$da= getdate();
-	$year = date ("Y", mktime (0,0,0,$da['mon'],$da['mday'],$da['year']));
+	for ( $i = 1; $i <= 31; $i++ ) {
+		$num = $i < 10 ? '0'.$i : $i;
+		$item .= '<option value="'.$num.'" '.($num==$dd?"selected":"").'>'.$num.'</option>';
+	} $item .= '</select>';
+
+
+	// Year Chooser
+	$_5_years_ago = new DateTime();
+	$_5_years_ago->modify('-5 year');
+	$year = $_5_years_ago->format('Y');
+	
 	$item .= '<select name="'.$tag.'_year" style="margin-left:10px;">';
-	for($i=1; $i<=3; $i++){
-		$check='';if($year == $yy){$check='selected';}
-		$item .= '<OPTION value="'.$year.'" '.$check.'>'.$year.'</OPTION>';
-		$year++;
+	for ( $i = 1; $i <= 9; $i++, $year++ ) {
+		$item .= '<option value="'.$year.'" '.($year==$yy?"selected":"").'>'.$year.'</option>';
+	} $item .= '</select>&nbsp;&nbsp;&nbsp;&nbsp;at ';	
+
+
+	// Make sure time is multiple of 15
+	$minutes = $date->format("i");
+	if ( $minutes % 15 > 0 ) {
+		$min = 15 - ($minutes % 15);
+		$date->modify('+ '.$min." minutes");
+		$mm = $date->format('m');
 	}
-	$item .= '</select>&nbsp;&nbsp;&nbsp;&nbsp;at ';	
-// ==================================================================
-// ========================== (Time) ================================
+
+	// Time chooser
 	$item .= '<select name="'.$tag.'_time" style="margin-left:10px;">';
-	for($i=0; $i<24; $i++){
-		$ti=explode(':',$current_time);
-		$da= getdate();
-		if($current_time){
-			$ctime = date ("H:i", mktime ($ti[0],$ti[1],0,$da['mon'],$da['mday'],$da['year']));
-		}
-		$ftime = date ("H:i", mktime ($i,0,0,$da['mon'],$da['mday'],$da['year']));
-		$time = date ("h:i A", mktime ($i,0,0,$da['mon'],$da['mday'],$da['year']));
-		$check='';if($ctime == $ftime){$check='selected';}
-		$item .= '<OPTION value="'.$ftime.'" '.$check.'>'.$time.'</OPTION>';
+	for ( $i = 0; $i < 24; $i++ ) {
 		
-		$time = date ("h:i A", mktime ($i,30,0,$da['mon'],$da['mday'],$da['year']));
-		$ftime = date ("H:i", mktime ($i,30,0,$da['mon'],$da['mday'],$da['year']));
-		$check='';if($ctime == $ftime){$check='selected';}
-		$item .= '<OPTION value="'.$ftime.'" '.$check.'>'.$time.'</OPTION>';
-	}
-	$item .= '</select>';
-// ==================================================================
+		$ctime = $date->format("H:i");
+	
+		$ftime = date("H:i", mktime ($i,0,0,$mm,$dd,$yy));
+		$time = date("h:i A", mktime ($i,0,0,$mm,$dd,$yy));
+		$item .= '<option value="'.$ftime.'" '.($ctime==$ftime?"selected":"").'>'.$time.'</option>';
+		
+		$time = date ("h:i A", mktime ($i,15,0,$mm,$dd,$yy));
+		$ftime = date ("H:i", mktime ($i,15,0,$mm,$dd,$yy));
+		$item .= '<option value="'.$ftime.'" '.($ctime==$ftime?"selected":"").'>'.$time.'</option>';
+		
+		$time = date ("h:i A", mktime ($i,30,0,$mm,$dd,$yy));
+		$ftime = date ("H:i", mktime ($i,30,0,$mm,$dd,$yy));
+		$item .= '<option value="'.$ftime.'" '.($ctime==$ftime?"selected":"").'>'.$time.'</option>';
+		
+		$time = date ("h:i A", mktime ($i,45,0,$mm,$dd,$yy));
+		$ftime = date ("H:i", mktime ($i,45,0,$mm,$dd,$yy));
+		$item .= '<option value="'.$ftime.'" '.($ctime==$ftime?"selected":"").'>'.$time.'</option>';
+		
+	} $item .= '</select>';
+
 ?>
