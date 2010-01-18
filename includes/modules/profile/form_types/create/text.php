6e2dc46a91c49a00f36e1d57e4a1a6c1d28aa9b6
Matt Brewer <matt.brewer@me.com>
2010-01-18 18:02:24 -0500

    Updated "text" form_type in profile module to correctly strip slashes and handle quotes

--- includes/modules/profile/form_types/create/text.php
+++ includes/modules/profile/form_types/create/text.php
@@ -4,7 +4,7 @@
 	if ( $form_array['default'] && !$value ) {
 		$item = '<input id="'.$id.'" type="text" name="'.$form_array['name'].'" value="'.$form_array['default'].'" size="'.$form_array['size'].'" maxlength="'.$form_array['max'].'" class="'.$form_array['class'].'" style="'.$form_array['style'].'">';
 	} else {
-		$item = '<input id="'.$id.'" type="text" name="'.$form_array['name'].'" value="'.$value.'" size="'.$form_array['size'].'" maxlength="'.$form_array['max'].'" class="'.$form_array['class'].'" style="'.$form_array['style'].'">';
+		$item = '<input id="'.$id.'" type="text" name="'.$form_array['name'].'" value="'.htmlentities(stripslashes($value)).'" size="'.$form_array['size'].'" maxlength="'.$form_array['max'].'" class="'.$form_array['class'].'" style="'.$form_array['style'].'">';
 	}
 	
 ?>
