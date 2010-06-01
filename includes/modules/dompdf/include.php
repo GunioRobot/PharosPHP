6aaebcd015106151386ad68a2a6dcb416247027a
Matt Brewer <matt.brewer@me.com>
2010-06-01 17:09:00 -0400

    Updated the "dompdf" module to v1.1

--- includes/modules/dompdf/include.php
+++ includes/modules/dompdf/include.php
@@ -2,5 +2,6 @@
 
 	// Simple setup, load in all function files (injecting into application)
 	require_once dirname(__FILE__).'/functions/autoload.php';
+	require_once dirname(__FILE__).'/library/dompdf_config.inc.php';
 	
 ?>
--- includes/modules/dompdf/version.php
+++ includes/modules/dompdf/version.php
@@ -1,6 +1,8 @@
 <?
 
 	define('DOMPDF_VERSION_MAJOR', 1);
-	define('DOMPDF_VERSION_MINOR', 0);
+	define('DOMPDF_VERSION_MINOR', 1);
+	
+	$module_version = "1.1";	// For newer CMSLite version comparison
 	
 ?>
