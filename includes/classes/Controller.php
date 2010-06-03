48c5d753e01f884c6bdc0f06aad9188160eadb75
Matt Brewer <matt.brewer@me.com>
2010-06-02 21:37:08 -0400

    Controller::loadModule() is now a symlic to Modules::load()

--- includes/classes/Controller.php
+++ includes/classes/Controller.php
@@ -160,63 +160,7 @@
 		//
 		///////////////////////////////////////////////////////////////////////////
 		public static function loadModule($name) {
-			if ( !isset(self::$modules[$name]) ) {
-				$folder = MODULES_DIR;
-				if ($handle = opendir($folder)) {
-					while (false !== ($file = readdir($handle)) ) {
-						if ($file != "." && $file != ".." && is_dir($folder.$file) && $file === $name ) {
-							if ( @file_exists($folder.$file.'/include.php') ) {
-								
-								// Make sure the version of the module is correct, then load
-								if ( @file_exists($folder.$file.'/version.php') ) {
-									
-									unset($module_version);
-									include $folder.$file.'/version.php';
-									if ( isset($module_version) ) {
-										
-										include $folder.$file.'/include.php';
-										$modules[$name] = $folder.$file.'/include.php';
-										
-										// Try to validate using the newer string version comparison instead of nasty defines
-										if ( ($comp = version_compare($module_version, CMS_VERSION)) >= 0 ) {
-											Console::log("Loaded ($name) successfully");
-											call_hook(Hooks::HOOK_MODULE_LOADED, array($name));
-										} else {
-											$err = "Unable to load ($name) -- Incorrect Version (".$module_version." < ".CMS_VERSION.")";
-											Console::log($err);
-										}
-										
-									} else {
-									
-										// Try using the defines method as a failsafe for older CMSLite installs
-										$upperName = strtoupper($name);
-										if ( defined($upperName."_VERSION_MAJOR") && defined($upperName."_VERSION_MINOR") ) {
-
-											include $folder.$file.'/include.php';
-											$modules[$name] = $folder.$file.'/include.php';
-
-											if ( constant($upperName."_VERSION_MAJOR") >= CMS_VERSION_MAJOR && constant($upperName."_VERSION_MINOR") >= CMS_VERSION_MINOR ) {
-												Console::log("Loaded ($name) successfully");
-												Hooks::call_hook(Hooks::HOOK_MODULE_LOADED, array($name));
-											} else {
-												$err = "Unable to load ($name) -- Incorrect Version (".constant($upperName."_VERSION_MAJOR").".".constant($upperName."_VERSION_MINOR")." < ".CMS_VERSION_MAJOR.".".CMS_VERSION_MINOR.")";
-												Console::log($err);
-											}
-
-										} else {
-											$err = "Unable to load ($name) -- Missing versioning information";
-											throw new Exception($err);
-										}
-										
-									}
-																		
-								}
-								
-							}
-						}
-					}
-				}
-			}
+			Modules::load($name);
 		}
 		
 		
