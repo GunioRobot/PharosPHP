286f03d40eade341d62d34454604c4d6abe6fae1
Matt Brewer <matt.brewer@me.com>
2010-06-02 20:31:37 -0400

    Updated Session class to that used in CoreLabs (using template files for and better pages for password reset functionality)

--- includes/controllers/Session.php
+++ includes/controllers/Session.php
@@ -5,7 +5,6 @@
 		public function __construct() {
 			
 			parent::__construct();
-			$this->name = "Session Manager";
 			$this->title = "Login";
 						
 		}
@@ -37,9 +36,7 @@
 				if ( $info->fields['user_id'] ) {
 
 					// Info needed by system
-					$_SESSION['domain_id'] = DOMAIN_ID;
-					$_SESSION['pws'] = $info->fields['user_password'];
-					$_SESSION['signin'] = $info->fields['user_username'];
+					$_SESSION['domain_id'] = SECURE_KEYWORD;
 					$_SESSION['uid'] = $info->fields['user_id'];
 					$_SESSION['user_level'] = $info->fields['user_level'];
 					$_SESSION['fullname'] = $info->fields['user_first_name'] . ' ' . $info->fields['user_last_name'];
@@ -48,11 +45,15 @@
 					$this->db->Execute("UPDATE users SET user_last_login = NOW() WHERE user_id = '".$info->fields['user_id']."' LIMIT 1");
 
 					// Finish redirecting
-					redirect('/'.ADMIN_DIR);
+					redirect(site_link());
 				}
+				
+				$loginMessage = "Incorrect username/password combination.";
+				require_once TEMPLATE_DIR.'views/login-view.php';
+							
 			} else {
 				
-				// Special, b/c the system shows the login page and doesn't use a provided view
+				require_once TEMPLATE_DIR.'views/login-view.php';				
 				
 			}			
 		
@@ -63,8 +64,6 @@
 			$this->title = "Logout";
 			
 			unset($_SESSION['domain_id']);
-			unset($_SESSION['pws']);
-			unset($_SESSION['signin']);
 			unset($_SESSION['uid']);
 			unset($_SESSION['login_type']);
 			unset($_SESSION['user_level']);
@@ -72,6 +71,46 @@
 			
 			redirect(site_link());
 			
+		}
+	
+		
+		public function passwordReset() {
+			$this->title = "Reset Password";
+			require_once TEMPLATE_DIR.'views/password-reset-view.php';
+		}
+
+
+		public function processPasswordReset() {
+			if ( ($user = post('user')) !== false ) {
+				reset_password($user);
+				exit;
+			} else {
+				redirect(controller_link(__CLASS__, "password-reset/"));
+			}
+		}
+
+
+		public function passwordSuccessfullyReset() {
+
+			$this->title = 'Password Successfully Reset';
+			$message = 'Your password was successfully reset.<br /><br />';
+			$message .= 'Check the email account you registered with for your new password and instructions.';
+			require_once TEMPLATE_DIR.'views/password-reset-finished-view.php';
+
+		}
+
+		public function passwordResetFailed($code="1") {
+
+			if ( $code == 1 ) {
+				$this->title = 'Unable to Reset Password';
+				$message = 'We were unable to reset your password because either the username provided is not in the system or there is not a valid email address for this user.';
+			} else {
+				$this->title = 'Unable to Send Email';
+				$message = 'We were unable to send an email to the email address specified in your profile.<br />Please contact the <a href="mailto:'.SYS_ADMIN_EMAIL.'subject?Failed Password Reset">system administrator</a> for help.';
+			}
+
+			require_once TEMPLATE_DIR.'views/password-reset-finished-view.php';
+
 		}
 	
 	}
