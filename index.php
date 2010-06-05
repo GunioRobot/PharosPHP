1edcf20eeb2505c2049a255b31f169bf7cf7f2f1
Matt Brewer <matt.brewer@me.com>
2010-06-05 12:27:13 -0400

    Controller class now handles storing output buffer contents during finalize()

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?
 		
-	// Site includes
-	require_once 'includes/app_header.php';
+	// Begin loading the system
+	require_once 'system/init.php';
 	
 	// Validate login information
 	validate_login();
-			
+		
+	// System action to allow post system-init, pre controller created actions to execute		
 	Hooks::call_hook(Hooks::HOOK_CONTROLLER_PRE_CREATED);	
-					
+	
+	// The Router has parsed for the class to load, attempt to load it				
 	$controllerClass = Router::controller();
 	$file = CONTROLLER_DIR.$controllerClass.'.php';		
 	if ( file_exists($file) ) {
@@ -17,17 +19,19 @@
 		require_once $file;
 		$controller = new $controllerClass();
 		Hooks::call_hook(Hooks::HOOK_CONTROLLER_POST_CREATED, array($controllerClass));	
-				
+		
+		// Call a method on the class (determined by the Router) & capture the output		
 		$method = Router::method();	
 		if ( method_exists($controller, $method) ) {
 			call_user_func_array(array($controller, $method), Router::params());
 		} else {
 			Console::log("Unknown method (".$method.") for class($controllerClass)");
 		}	
+
+		// Grab the contents of the buffer & give to controller to use. Turn off buffering as well
+		$controller->finalize(ob_get_flush());
 		
-		if ( $controller->output() === "" ) $controller->output(ob_get_contents());
-		ob_end_clean();
-		
+		// Render the template to the browser
 		render_template();
 			
 	} else {
