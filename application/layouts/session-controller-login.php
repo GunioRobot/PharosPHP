62adef68ada846b5446869c12a2e7117824df8e2
Matt Brewer <matt.brewer@me.com>
2010-06-06 16:32:56 -0400

    This layout overrides the application layout on the login screen

---
+++ application/layouts/session-controller-login.php
@@ -0,0 +1,20 @@
+
+<? require_once LAYOUTS_DIR.'tpl_HTML_header.php'; ?>
+
+	<div id="bodyWrapper">
+
+		<div id="DCheader">
+
+
+		    <a id="logo" href="<?=site_link()?>" title="Home"></a>
+	    	<div class="clearBoth"></div>
+	        <br clear="all" />
+	
+		</div>
+	    
+<?
+
+	require_once LAYOUTS_DIR.'tpl_body.php';
+	require_once LAYOUTS_DIR.'tpl_footer.php';
+	
+?>
