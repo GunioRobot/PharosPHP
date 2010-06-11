dd6b614da70b42507e2c075c4d2633939083bf13
Matt Brewer <matt.brewer@me.com>
2010-06-11 11:40:26 -0400

    Router class now correctly supports user defined :arg arguments in custom routing. All actions should now expect a single $params array instead of named arguments as before

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?
-		
+
 	// Begin loading the system
 	require_once 'system/init.php';
 	
 	// Validate login information
 	validate_login();
-			
+		
 	// System action to allow post system-init, pre controller created actions to execute		
 	Hooks::call_hook(Hooks::HOOK_CONTROLLER_PRE_CREATED);	
-	
+				
 	// The Router has parsed for the class to load, attempt to load it				
 	$controllerClass = Router::controller();
-	$file = CONTROLLER_DIR.$controllerClass.'.php';		
+	$file = CONTROLLER_DIR.$controllerClass.'.php';	
 	if ( file_exists($file) ) {
 		
 		ob_start();
@@ -23,7 +23,7 @@
 		// Call a method on the class (determined by the Router) & capture the output		
 		$method = Router::method();	
 		if ( method_exists($controller, $method) ) {
-			call_user_func_array(array($controller, $method), Router::params());
+			call_user_func(array($controller, $method), Router::params());
 		} else {
 			Console::log("Unknown method (".$method.") for class($controllerClass)");
 		}	
--- system/classes/Router.php
+++ system/classes/Router.php
@@ -23,9 +23,29 @@
 
 		private static $raw = "";
 		private static $components = array();
+		private static $routes = array();
+		private static $keys = array(":controller", ":action", ":id");
 		
 		public static function parse() {
 			
+			// Grab the defined routes
+			self::$routes = Settings::get("routes.connections");
+			foreach(self::$routes as &$route) {
+				
+				// Escape the pattern & apply the 3 default filters of (:controller, :action, & :id)
+				$route['pattern'] = str_replace("#", "\#", $route['pattern']);		// Escape the reserved '#' char
+				$route['parsed_pattern'] = str_replace(array(':controller', ':action'), '([[:alnum:]]+)', $route['pattern']);	// 
+				$route['parsed_pattern'] = str_replace(':id', '([[:digit:]]+)', $route['parsed_pattern']);
+				
+				// Apply the user defined filters
+				foreach($route['params'] as $key => $pattern) {
+					$route['parsed_pattern'] = str_replace($key, $pattern, $route['parsed_pattern']);
+				}
+								
+			}
+						
+			
+			// Grab the input string, parsing the pieces into the $components array (which is used to decide the :controller, :action & :arguments)
 			if ( ($input = get("args")) !== false ) {
 				
 				self::$raw = trim($input, " /");
@@ -42,31 +62,67 @@
 			
 		}
 		
+		
+
+		
 		public static function controller() {
-			$c = !empty(self::$components) ? self::$components[0]."Controller" : Settings::get('routes.root.controller');
-			return controller_name($c);
+									
+			// Attempt to find a match based upon the custom routing defined
+			if ( ($route = self::_matching_route()) === false ) {
+			
+				// Didn't have custom routing option setup, so use basic routing (the first portion of URL) or the root controller defined			
+				$c = !empty(self::$components) ? self::$components[0]."Controller" : Settings::get('routes.root.controller');
+				return controller_name($c);
+				
+			} else {
+				if ( $route['param_values'][':controller'] != "" ) return controller_name($route['param_values'][':controller'])."Controller";
+				else return $route['controller'];
+			}
+			
 		}
+		
+		
+		
 		
 		public static function method() {
 			
-			$m = count(self::$components) > 1 ? controller_name(self::$components[1]) : "index";
-			if ( Router::controller() == Settings::get('routes.root.controller') ) {
-				try {
+			// Attempt to find a match based upon the custom routing defined
+			if ( ($route = self::_matching_route()) === false ) {
+								
+				$m = count(self::$components) > 1 ? controller_name(self::$components[1]) : "index";
+				if ( Router::controller() == Settings::get('routes.root.controller') ) {
+					try {
 				
-					$method = Settings::get('routes.root.action');
-					return $method;
+						$method = Settings::get('routes.root.action');
+						return $method;
 				
-				} catch (Exception $e) {
-					return $m;
+					} catch (Exception $e) {
+						return $m;
+					}
 				}
+			
+				return $m;
+				
+			} else {
+				if ( $route['param_values'][':action'] != "" ) return controller_name($route['param_values'][':action']);
+				else return controller_name($route['action']);
 			}
 			
-			return $m;
 		}
 		
+		
+		
+		
 		public static function params() {
-			return count(self::$components) > 2 ? array_slice(self::$components,2) : array();
+			
+			if ( ($route = self::_matching_route()) !== false ) {
+				return array_diff_key($route['param_values'], array(":controller" => true, ":action" => true));				
+			} else return count(self::$components) > 2 ? array_slice(self::$components,2) : array();
+			
 		}
+		
+		
+		
 		
 		public static function requires_login() {
 			
@@ -75,9 +131,13 @@
 			
 		}
 		
+		
+		
 		public static function raw_input() {
 			return self::$raw;
 		}
+		
+		
 		
 		public static function layout() {
 			
@@ -95,10 +155,52 @@
 		}
 		
 		
+		
+		
 		private static function _layout_file($class) {
 			return strtolower(implode('-',split_camel_case($class)));
 		}
-	
+		
+		
+		private static function _matching_route() {
+			
+			static $route = null;
+			if ( $route != null ) return $route;
+									
+			foreach(self::$routes as $r) {
+							
+				// Check to see if the URL matched - if it does, $matches will contain the captured sequences
+				$matches = array();
+				if ( preg_match('#'.$r['parsed_pattern'].'#', self::$raw, $matches) ) {
+					
+					$matches = array_slice($matches,1);		// Get rid of first element
+					
+					// Now find the sequence of the captures
+					$search_keys = array_merge(self::$keys, array_keys($r['params']));
+					$keys = array();
+					foreach($search_keys as $key) {
+						$pos = strpos($r['pattern'], $key);
+						if ( $pos !== false ) {
+							$keys[$pos] = $key;
+						}
+					}
+					
+					ksort($keys);
+										
+					$route = $r;
+					$route['param_values'] = array_combine($keys, $matches);
+						
+					return $route;
+					
+				}
+				
+			}
+			
+			$route = false;
+			return $route;
+			
+		}
+			
 	}
 
 ?>
