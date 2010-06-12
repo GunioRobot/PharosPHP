2b21a60f88ed9a996cab306d32f8ad304f3de5d2
Matt Brewer <matt.brewer@me.com>
2010-06-11 21:36:16 -0400

    Moved a lot of the output related functionality to the Output class

--- system/classes/Controller.php
+++ system/classes/Controller.php
@@ -8,24 +8,17 @@
 	//
 	///////////////////////////////////////////////////////////////////////////
 
+	require_once CLASSES_DIR.'Output.php';
 	class Controller {
 		
-		const CSS_TYPE_ALL = "all";
-		const CSS_TYPE_PRINT = "print";
-		const CSS_TYPE_SCREEN = "screen";
-		const JAVASCRIPT_INCLUDE = "php_include_js";
-		const JAVASCRIPT_EXTERNAL = "link_js";
 	
 		///////////////////////////////////////////////////////////////////////////
 		//
 		//	Available to this class and all subclasses
 		//
 		///////////////////////////////////////////////////////////////////////////
-		protected $css;
-		protected $javascript;
-		protected $output;
 		protected $db;
-		protected $meta = array();
+		
 		
 		///////////////////////////////////////////////////////////////////////////
 		//
@@ -36,168 +29,29 @@
 		public $title;
 		public $keywords;
 		public $description;
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	self::$modules[$name] for loaded modules in the system
-		//
-		///////////////////////////////////////////////////////////////////////////
-		protected static $modules = array();
+		public $output;
 		
 		
 		
 		///////////////////////////////////////////////////////////////////////////
 		//
-		//	Constructor, optionally takes array of CSS and JS files
+		//	Constructor
 		//
 		///////////////////////////////////////////////////////////////////////////
-		public function __construct($css=array(),$javascript=array()) {
+		public function __construct() {
 			
 			global $db;
-		
-			$this->css = ( is_array($css) ) ? $css : array();
-			$this->javascript = ( is_array($javascript) ) ? $javascript : array();
-			
+				
 			$this->db =& $db;
+			$this->output = new Output();
 						
 			$this->name = get_class($this);
 			$this->title = "";
 			$this->keywords = DEFAULT_KEYWORDS;
 			$this->description = DEFAULT_DESCRIPTION;
-			
-			$this->output = "";
-		
-		}
-		
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	Either retuns the output, or sets the output if given input
-		//
-		///////////////////////////////////////////////////////////////////////////
-		public function output($string='') {
-			if ( $string != '' ) {
-				$this->output = $string;
-			} else {
-				return $this->output;
-			}
-		}
-		
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	Either returns array, or adds a stylesheet to be used
-		//
-		///////////////////////////////////////////////////////////////////////////
-		public function css($path='', $type=self::CSS_TYPE_ALL) {
-			
-			if ( $path != '' ) {
-				
-				// Don't keep adding the same thing
-				if ( in_array($path,$this->css) ) {
-					return false;
-				}
-			
-				switch($type) {
-					case self::CSS_TYPE_ALL:
-					case self::CSS_TYPE_PRINT:
-					case self::CSS_TYPE_SCREEN:
-						break;
-					default:
-						$type = self::CSS_TYPE_ALL;
-						break;
-				}
-			
-				$this->css[] = array('path' => $path, 'type' => $type);
-				
-			} else {
-				return $this->css;
-			}
-			
-		}
-	
-	
-	
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	Returns array of javascript files, or if given input, adds to array
-		//	If given same path for 2nd time, just updates $data array
-		//
-		///////////////////////////////////////////////////////////////////////////
-		public function javascript($path='',$data=array()) {
-			
-			if ( !is_array($data) ) $data = array();
-		
-			if ( $path != '' ) {
-				
-				// Don't keep adding the same thing
-				if ( ($index = array_search($path,$this->javascript)) !== false ) {
 					
-					// Replace data array if called a second time
-					$this->javascript[$index]['data'] = $data;
-					return true;
-					
-				} else {
-					$this->javascript[] = array('path' => $path, 'type' => (strrpos($path,'.php')===false?self::JAVASCRIPT_EXTERNAL:self::JAVASCRIPT_INCLUDE), 'data' => $data);
-				}
-				
-			} else {
-				
-				return $this->javascript;
-				
-			}
-			
 		}
-		
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	Setter/getter for meta information. Providing an array of 
-		//		- name
-		//		- content
-		//		- http-equiv
-		//
-		///////////////////////////////////////////////////////////////////////////
-		
-		public function meta($meta=null) {
-			if ( !is_null($meta) ) {
-				$this->meta[$meta['name']] = $meta;
-			} else return $this->meta;
-		}
-		
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	Called by index.php, sets the $output var conditionally
-		//
-		///////////////////////////////////////////////////////////////////////////
-	
-		public function finalize($output) {
-			if ( $this->output() === "" ) $this->output($output);
-		} 
-				
-		
-		///////////////////////////////////////////////////////////////////////////
-		//
-		//	When destroying the object
-		//
-		///////////////////////////////////////////////////////////////////////////
-		public function __destruct() {
-					
-			unset($this->css);
-			unset($this->javascript);
-			
-			if ( class_exists("Console") ) {
-				Console::log("Unloading (".get_class($this).")");
-			}
-		
-		}
-	
+
 	}
 	
 ?>
