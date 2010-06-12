230bb6f18207020648dad29f577e0e7827498940
Matt Brewer <matt.brewer@me.com>
2010-06-11 21:42:25 -0400

    Removed use of explicitly setting value of output to browser

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -137,7 +137,7 @@
 		          </div>'.
 		          $this->table->get_html($this->table->current_page, $page_count, $start, $total);
 
-			$this->output($view);
+			echo $view;
 
 		}
 		
@@ -198,8 +198,8 @@
 			// Run throught the parser and spit out the page
 			$profile = new Profile($fields);
 			
-			if ( $id > 0 ) $this->output($profile->display($this->dataKey, $id, $repost));
-			else $this->output($profile->display());
+			if ( $id > 0 ) echo $profile->display($this->dataKey, $id, $repost);
+			else echo $profile->display();
 						
 		}
 		
