86d2bed625487fe85b8c20a8f084641d30e50638
Matt Brewer <matt.brewer@me.com>
2010-06-11 21:44:19 -0400

    Updated to not explicitly set output to browser

--- application/controllers/NavigationController.php
+++ application/controllers/NavigationController.php
@@ -118,7 +118,7 @@
 		          </div>'.
 		          $this->table->get_html($this->table->current_page, $page_count, $start, $total);
 
-			$this->output($view);
+			echo $view;
 
 		}
 		
@@ -169,8 +169,8 @@
 			// Run throught the parser and spit out the page
 			$profile = new Profile($fields);
 			
-			if ( $id > 0 ) $this->output($profile->display($this->dataKey, $id, $repost));
-			else $this->output($profile->display());
+			if ( $id > 0 ) echo $profile->display($this->dataKey, $id, $repost);
+			else echo $profile->display();
 						
 		}
 		
