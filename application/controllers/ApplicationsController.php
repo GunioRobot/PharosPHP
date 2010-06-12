c6542db518a94d7a9eaa373f5f784c053e3d0473
Matt Brewer <matt.brewer@me.com>
2010-06-11 21:45:45 -0400

    Using 'echo' to push content to browser

--- application/controllers/ApplicationsController.php
+++ application/controllers/ApplicationsController.php
@@ -139,7 +139,7 @@
 		          </div>'.
 		          $this->table->get_html($this->table->current_page, $page_count, $start, $total);
 
-			$this->output($view);
+			echo $view;
 
 		}
 		
@@ -208,8 +208,8 @@
 			// Run throught the parser and spit out the page
 			$profile = new Profile($fields);
 			
-			if ( $id > 0 ) $this->output($profile->display($this->dataKey, $id, $repost));
-			else $this->output($profile->display());
+			if ( $id > 0 ) $profile->display($this->dataKey, $id, $repost);
+			else echo $profile->display();
 						
 		}
 		
