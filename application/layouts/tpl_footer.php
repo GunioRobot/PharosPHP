3f04dedb19ab4073510336d3b76387fb5a61f4f4
Matt Brewer <matt.brewer@me.com>
2010-06-14 10:33:33 -0400

    Removed call to is_logged_in()

--- application/layouts/tpl_footer.php
+++ application/layouts/tpl_footer.php
@@ -11,7 +11,7 @@
 	
 	</div>
 	
-	<? if ( is_logged_in() && SHOW_PROFILER_RESULTS ) {global $profiler; $profiler->display($db);} ?>
+	<? if ( SHOW_PROFILER_RESULTS ) {global $profiler; $profiler->display($db);} ?>
 	
 	<? Hooks::call_hook(Hooks::HOOK_TEMPLATE_FOOTER) ?>
 
