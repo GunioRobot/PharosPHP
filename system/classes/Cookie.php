7c37bf7293566beb50653af6c8fd56a431b4cae6
Matt Brewer <matt.brewer@me.com>
2010-06-18 09:24:44 -0400

    Added ::delete() method to remove a cookie, and updated ::set() to store immediately in $_COOKIE so the value is available on the same page load

--- system/classes/Cookie.php
+++ system/classes/Cookie.php
@@ -11,11 +11,16 @@
 	class Cookie {
 		
 		public static function set($key, $value, $expire) {
+			$_COOKIE[$key] = $value;	// Make it available on this same page load, instead of requiring reload to fill $_COOKIE as PHP behavior dictates
 			return @setcookie($key, $value, $expire, "/", HTTP_SERVER, false, false);
 		}
 		
 		public static function get($key, $default=false) {
 			return cookie($key, $default);
+		}
+		
+		public static function delete($key) {
+			return @setcookie($key, "", time() - 3600);
 		}
 		
 	} 
