f8ee3deb23fa985f6ebd06b56b12ad82d4628172
Matt Brewer <matt.brewer@me.com>
2010-06-23 19:27:36 -0400

    Updated documentation

--- system/init.php
+++ system/init.php
@@ -1,24 +1,21 @@
 <?
 
 	session_start();
-	
-	$f = dirname(__FILE__);
-	
+		
 	// Two VERY important defines, used everywhere
+	$f = dirname(__FILE__);
 	define('APP_PATH', substr(dirname($_SERVER['SCRIPT_NAME']), 1).'/');	
 	define('SERVER_DIR', substr(substr($f, 0, strrpos($f, "/"))."/", 0, -strlen(APP_PATH)));
 	
 	// Load in all the functions (.php files) in this folder
 	require_once SERVER_DIR.APP_PATH.'system/functions/autoload.php';
 	
-	// Define Global Vars and Config Information
+	// Must be the first 2 classes loaded in the system
 	require_once SERVER_DIR.APP_PATH.'system/classes/YAML/sfYaml.php';
 	require_once SERVER_DIR.APP_PATH.'system/classes/Settings.php';
 	
-	// Load the Router and request parsing of the URL to controller class, method, and params	
+	// Now load the remaining core classes
 	require_once CLASSES_DIR.'Router.php';
-		
-	// Load the classes that are barebones
 	require_once CLASSES_DIR.'Template.php';
 	require_once CLASSES_DIR.'QueryFactory.php';
 	require_once CLASSES_DIR.'Controller.php';
