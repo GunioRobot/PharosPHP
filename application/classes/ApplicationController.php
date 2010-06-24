11bf3a639ace94da7e995c676c500d5eef5aa925
Matt Brewer <matt.brewer@me.com>
2010-06-24 15:24:28 -0400

    Added ApplicationController

---
+++ application/classes/ApplicationController.php
@@ -0,0 +1,14 @@
+<?
+
+	/**
+	 * ApplicationController
+	 * Extend this class to provide functionality for all of your application controllers to use
+	 *
+	 * @package PharosPHP.Application.Classes
+	 * @author Matt Brewer
+	 **/
+	class ApplicationController extends Controller {
+		
+	} 
+
+?>
