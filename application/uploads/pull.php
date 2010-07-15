effbf980e543b229744d46bcc36e44f5c9f7f9e1
Matt Brewer <matt.brewer@me.com>
2010-07-15 12:16:32 -0400

    pull.php updated to respect "store_filesize" & "store_filetype" options now

--- application/uploads/pull.php
+++ application/uploads/pull.php
@@ -24,8 +24,8 @@
 			
 				$info = pathinfo($filename);
 			
-				$filesize = !request("store_filesize") ? "" : " $name"."_file_size = '".filesize($filename)."', ";	
-				$filetype = !request("store_file_type") ? "" : " $name"."_file_type = '".$info['extension']."', ";	
+				$filesize = request("store_filesize",'false') == 'true' ? " $name"."_file_size = '".filesize($filename)."', " : '';	
+				$filetype = request("store_file_type",'false') == 'true' ? " $name"."_file_type = '".$info['extension']."', " : '';	
 					
 				$sql = "UPDATE $table SET $name = '".$db->prepare_input($filename)."', $filesize $filetype last_updated = NOW() WHERE $key = '".(int)$id."' LIMIT 1";
 				$db->Execute($sql);
