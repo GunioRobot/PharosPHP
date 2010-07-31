2b352d9cf9fb78df99e79e9ccce4789c03e8d246
Matt Brewer <matt.brewer@me.com>
2010-07-30 22:39:42 -0400

    Added InvalidConnectionSettingsException

---
+++ system/classes/Exceptions/InvalidConnectionSettingsException.php
@@ -0,0 +1,14 @@
+<?
+
+	/**
+	 * InvalidConnectionSettingsException
+	 *
+	 * @package PharosPHP.Core.Classes.Exceptions
+	 * @author Matt Brewer
+	 **/
+
+	class InvalidConnectionSettingsException extends Exception {
+		protected $message = "Invalid Key Path Specified";
+	}
+
+?>
