27553c3e12fc0f3b7847fc952bec846effa85751
Matt Brewer <matt.brewer@me.com>
2010-08-06 08:56:24 -0400

    Added static function for loading classes, and Loader->class($name) is a convenience method for Loader::load_class($name)

---
+++ system/classes/Loader.php
@@ -0,0 +1,114 @@
+<?
+
+	/**
+	 * Loader
+	 *
+	 * This class is initialized and used as part of the Controller class, ie 
+	 * 
+	 * 		public function my_function() {
+	 *			try {
+	 *				$this->load->class("MyClass");
+	 *			} catch (ClassNotFoundException $e) {}
+	 * 		} 
+	 *
+	 * @package PharosPHP.Core.Classes
+	 * @author Matt Brewer
+	 **/
+	
+	class Loader {
+		
+				
+		/**
+		 * module
+		 *
+		 * @param mixed $module_name (array of module names, or just one module name)
+		 *
+		 * @throws Exception - if module failed to load
+		 *
+		 * @return void
+		 * @author Matt Brewer
+		 **/
+		
+		public function module($name) {
+			Modules::load($name);
+		}
+		
+		
+		/**
+		 * class
+		 *
+		 * @param string $class_name
+		 *
+		 * @throws ClassNotFoundException();
+		 * 
+		 * @return void
+		 * @author Matt Brewer
+		 **/
+		
+		public function class($name) {
+			self::load_class($name);
+		}
+		
+		
+		/**
+		 * load_class
+		 *
+		 * @param string $class_name
+		 *
+		 * @throws ClassNotFoundException
+		 *
+		 * @return void
+		 * @author Matt Brewer
+		 **/
+
+		public static function load_class($name) {
+			
+			if ( !class_exists($name) ) {
+				
+				if ( !file_exists(CLASSES_DIR.$name.".php") ) {
+					
+					if ( !file_exists(APPLICATION_CLASSES_DIR.$name.".php") ) {
+						throw new ClassNotFoundException(sprintf("Unable to locate file containing class (%s)", $name));
+					} else {
+						
+						@include_once APPLICATION_CLASSES_DIR.$name.".php";
+						if ( !class_exists($name) ) {
+							throw new ClassNotFoundException(sprintf("Could not find class (%s): include '%s.php'", $name, APPLICATION_CLASSES_DIR.$name));
+						}
+						
+					}
+					
+				} else {
+				
+					@include_once CLASSES_DIR.$name.'.php';
+					if ( !class_exists($name) ) {
+						throw new ClassNotFoundException(sprintf("Could not find class (%s): include '%s.php'", $name, CLASSES_DIR.$name));
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		
+		/**
+		 * config
+		 *
+		 * @param string $filename
+		 *
+		 * @throws Exception
+		 * @throws InvalidFileSystemPathException
+		 *
+		 * @return void
+		 * @author Matt Brewer
+		 **/
+		
+		public function config($filename) {
+			Settings::load($filename);
+		}
+		
+		
+	}  
+
+?>
