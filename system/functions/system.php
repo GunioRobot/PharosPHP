6f7166be77406f732e8ab38120446bf60802ace5
Matt Brewer <matt.brewer@me.com>
2010-08-09 10:05:21 -0400

    Removed bootstrap_system() call

--- system/functions/system.php
+++ system/functions/system.php
@@ -1,38 +1,5 @@
 <?php
 	
-	
-	////////////////////////////////////////////////////////////////////////////////
-	//
-	//	app_bootstrap()
-	//
-	//	Loads the current application into the system
-	//
-	////////////////////////////////////////////////////////////////////////////////
-	
-	function bootstrap_system() {
-		
-		global $db, $CURRENT_APP_ID, $CURRENT_APP_NAME;
-		
-		Hooks::call_hook(Hooks::HOOK_SYSTEM_PRE_BOOTSTRAP);
-		
-		// Set the system timezone
-		date_default_timezone_set(Settings::get("application.system.timezone"));
-		
-		load_content_types();
-		Settings::load_dynamic_system_settings();
-		load_automatic_modules();
-						
-		$CURRENT_APP_ID = session("app_id", 1);
-				
-		$title = $db->Execute("SELECT app_name FROM applications WHERE app_id = '$CURRENT_APP_ID' LIMIT 1");
-		$CURRENT_APP_NAME = format_title($title->fields['app_name']);
-		
-		Hooks::call_hook(Hooks::HOOK_SYSTEM_POST_BOOTSTRAP);
-				
-	}	
-	
-
-	
 	////////////////////////////////////////////////////////////////////////////////
 	//
 	//	load_content_types() 
