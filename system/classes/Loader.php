623e42ebf58b47f482ede0ba695773b52407683a
Matt Brewer <matt.brewer@me.com>
2010-08-09 09:06:37 -0400

    Loader::load_class() now accepts a path (relative or absolute) to include a class file. Note that the basename (without extension) of the file must match the class file contained within

--- system/classes/Loader.php
+++ system/classes/Loader.php
@@ -63,28 +63,44 @@
 
 		public static function load_class($name) {
 			
-			if ( !class_exists($name) ) {
+			$info = pathinfo($name);
+			if ( $info['dirname'] == "." ) {
+			
+				if ( !class_exists($name) ) {
 				
-				if ( !file_exists(CLASSES_DIR.$name.".php") ) {
+					if ( !file_exists(CLASSES_DIR.$name.".php") ) {
 					
-					if ( !file_exists(APPLICATION_CLASSES_DIR.$name.".php") ) {
-						throw new ClassNotFoundException(sprintf("Unable to locate file containing class (%s)", $name));
-					} else {
+						if ( !file_exists(APPLICATION_CLASSES_DIR.$name.".php") ) {
+							throw new ClassNotFoundException(sprintf("Unable to locate file containing class (%s)", $name));
+						} else {
 						
-						@include_once APPLICATION_CLASSES_DIR.$name.".php";
-						if ( !class_exists($name) ) {
-							throw new ClassNotFoundException(sprintf("Could not find class (%s): include '%s.php'", $name, APPLICATION_CLASSES_DIR.$name));
+							@include_once APPLICATION_CLASSES_DIR.$name.".php";
+							if ( !class_exists($name) ) {
+								throw new ClassNotFoundException(sprintf("Could not find class (%s): include '%s.php'", $name, APPLICATION_CLASSES_DIR.$name));
+							}
+						
 						}
-						
-					}
 					
-				} else {
+					} else {
 				
-					@include_once CLASSES_DIR.$name.'.php';
-					if ( !class_exists($name) ) {
-						throw new ClassNotFoundException(sprintf("Could not find class (%s): include '%s.php'", $name, CLASSES_DIR.$name));
-					}
+						@include_once CLASSES_DIR.$name.'.php';
+						if ( !class_exists($name) ) {
+							throw new ClassNotFoundException(sprintf("Could not find class (%s): include '%s.php'", $name, CLASSES_DIR.$name));
+						}
 					
+					}
+				
+				}
+				
+			} else {
+				
+				$klass = $info['filename'];
+				if ( !class_exists($klass) ) {
+					@include_once $name;
+				}
+				
+				if ( !class_exists($klass) ) {
+					throw new ClassNotFoundException(sprintf("Attempted to load class (%s) from file: %s", $klass, $name));
 				}
 				
 			}
