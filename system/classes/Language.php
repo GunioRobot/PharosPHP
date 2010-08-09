adb23dd1b306145fcb58b162021c7584d5f471a4
Matt Brewer <matt.brewer@me.com>
2010-08-09 09:25:21 -0400

    Language API now defines Language->retrieve($path, $default) in addition to Language::lookup($path, $default, $language)

--- system/classes/Language.php
+++ system/classes/Language.php
@@ -37,21 +37,23 @@
 			}
 			
 			$key = $info['filename'];
+			$file = $info['basename'];
+			
 			if ( $info['dirname'] == "." ) {	// If a directory wasn't provided, infer one
 			
-				if ( !file_exists(APPLICATION_LANGUAGES_DIR.$filename) && !file_exists(LANGUAGES_DIR.$filename) ) {
-					throw new InvalidFileSystemPathException(sprintf("Language file does not exist: (%s)", $filename));
+				if ( !file_exists(APPLICATION_LANGUAGES_DIR.$file) && !file_exists(LANGUAGES_DIR.$file) ) {
+					throw new InvalidFileSystemPathException(sprintf("Language file does not exist: (%s)", $file));
 				}
 			
 				// Load the system language file first, so that if an application language file is found, the values defined there will override
-				if ( file_exists(LANGUAGES_DIR.$filename) ) {
-					$config = sfYaml::load(LANGUAGES_DIR.$filename);
+				if ( file_exists(LANGUAGES_DIR.$file) ) {
+					$config = sfYaml::load(LANGUAGES_DIR.$file);
 					self::$languages[$key] = is_array(self::$languages[$key]) ? array_merge(self::$languages[$key], $config) : $config;
 				}
 			
 				// Any values defined in /application/languages/{lang}.yml will override values defined for the system version
-				if ( file_exists(APPLICATION_LANGUAGES_DIR.$filename) ) {
-					$config = sfYaml::load(APPLICATION_LANGUAGES_DIR.$filename);
+				if ( file_exists(APPLICATION_LANGUAGES_DIR.$file) ) {
+					$config = sfYaml::load(APPLICATION_LANGUAGES_DIR.$file);
 					self::$languages[$key] = is_array(self::$languages[$key]) ? array_merge(self::$languages[$key], $config) : $config;
 				}
 							
@@ -72,26 +74,52 @@
 		
 		
 		/**
-		 * get
+		 * lookup
 		 *
 		 * @param (Keypath|string) $keypath
 		 * @param string $default_text (optional - used if value is not defined)
+		 * @param string $lang (optional - uses default_language already defined)
 		 *
+		 * @throws UnexpectedValueException
 		 * @throws InvalidKeyPathException
 		 *
-		 * @return void
+		 * @return mixed $value
 		 * @author Matt Brewer
 		 **/
 		
-		public static function get($path, $default="") {
+		public static function lookup($path, $default="", $lang=null) {
+			
+			if ( !is_string($lang) ) {
+				$lang = self::$current_language;
+			}
+			
+			if ( !isset(self::$languages[$lang]) ) {
+				throw new UnexpectedValueException(sprintf("Unexpected language: (%s)", $lang));
+			}
 			
 			if ( !($path instanceof Keypath) ) {
 				$path = new Keypath($path);
 			}		
 			
-			$value = $path->retrieve(self::$languages[self::$current_language]);
+			$value = $path->retrieve(self::$languages[$lang]);
 			return $value === Keypath::VALUE_UNDEFINED ? $default : $value;
 			
+		}
+		
+		
+		
+		/**
+		 * retrieve
+		 *
+		 * @param (Keypath|string) $keypath
+		 * @param string $default_text (optional - used if value is not defined)
+		 *
+		 * @return mixed $value
+		 * @author Matt Brewer
+		 **/
+		
+		public function retrieve($path, $default="") {
+			return self::lookup($path, $default, $this->language);
 		}
 		
 		
@@ -110,32 +138,14 @@
 		
 		
 		/**
-		 * get_by_lang
+		 * currentLanguage
 		 *
-		 * @param string $language
-		 * @param (Keypath|string) $keypath
-		 * @param string $default_text (optional - used if value is not defined)
-		 *
-		 * @throws UnexpectedValueException
-		 * @throws InvalidKeyPathException
-		 *
-		 * @return void
+		 * @return string $current_language
 		 * @author Matt Brewer
 		 **/
-		
-		public function get_by_lang($lang, $path, $default="") {
-			
-			if ( !isset(self::$languages[$lang]) ) {
-				throw new UnexpectedValueException(sprintf("Unexpected language: (%s)", $lang));
-			}
-			
-			if ( !($path instanceof Keypath) ) {
-				$path = new Keypath($path);
-			}		
 
-			$value = $path->retrieve(self::$languages[$lang]);
-			return $value === Keypath::VALUE_UNDEFINED ? $default : $value;
-		
+		public static function currentLanguage() {
+			return self::$current_langage;
 		}
 		
 	} 
