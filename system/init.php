bdad2b7ab4afca4573561746164637671924a06f
Matt Brewer <matt.brewer@me.com>
2010-08-09 09:27:19 -0400

    Reordered the initialization process to make Language API and other core functions available earlier to application code

--- system/init.php
+++ system/init.php
@@ -2,27 +2,62 @@
 
 	session_start();
 		
+		
+		
 	// Two VERY important defines, used everywhere
 	$f = dirname(__FILE__);
 	define('SERVER_DIR', $_SERVER['DOCUMENT_ROOT'].'/');
 	define('APP_PATH', substr(realpath($f."/../")."/", strlen(SERVER_DIR)));
 	
+	
+	
+	// Initialize some path information used throughout the system
+	define('SYSTEM_DIR', SERVER_DIR.APP_PATH.'system/');
+	define('APPLICATION_DIR', SERVER_DIR.APP_PATH.'application/');
+	
+	define('CLASSES_DIR', SYSTEM_DIR.'classes/');
+	define('FUNCTIONS_DIR', SYSTEM_DIR.'functions/');
+	define('EXCEPTIONS_DIR', CLASSES_DIR.'Exceptions/');
+	define('CACHE_DIR', APPLICATION_DIR.'cache/');	
+	define('PUBLIC_DIR', SERVER_DIR.APP_PATH.'public/');
+	
+	define('VIEWS_DIR', APPLICATION_DIR.'views/');	
+	define('LAYOUTS_DIR', APPLICATION_DIR.'layouts/');
+	define('MODELS_DIR', APPLICATION_DIR.'models/');
+	define('CONFIGURATION_DIR', APPLICATION_DIR.'configure/');
+	define('CONTROLLER_DIR', APPLICATION_DIR.'controllers/');
+	define('MODULES_DIR', APPLICATION_DIR.'modules/');
+	define('LANGUAGES_DIR', SYSTEM_DIR.'languages/');
+	
+	define('APPLICATION_CLASSES_DIR', APPLICATION_DIR.'classes/');
+	define('APPLICATION_FUNCTIONS_DIR', APPLICATION_DIR.'functions/');
+	define('APPLICATION_LANGUAGES_DIR', APPLICIATION_DIR.'languages/');
+		
+	
+	
+	// Load in all the exceptions used in the system
+	foreach(glob(EXCEPTIONS_DIR.'*.php') as $filename) {
+		require_once $filename;
+	} 	
+		
+		
+				
 	// Load in the few classes that are needed early on in system initialization
-	require_once SERVER_DIR.APP_PATH.'system/classes/Hooks.php';
-	require_once SERVER_DIR.APP_PATH.'system/classes/Loader.php';
+	require_once CLASSES_DIR.'Loader.php';
+	Loader::load_class('Hooks');
+	Loader::load_class('YAML/sfYaml.php');
+	Loader::load_class('Keypath');
+	Loader::load_class('Settings');	
 	
-	// Load in all the functions (.php files) in this folder
-	require_once SERVER_DIR.APP_PATH.'system/functions/autoload.php';
 	
-	autoload(EXCEPTIONS_DIR);				// Load in all the system defined Exception classes
-	autoload(APPLICATION_FUNCTIONS_DIR);	// Load in all the application defined functions
+	// Load in the system defined functions
+	foreach(glob(FUNCTIONS_DIR.'*.php') as $filename) {
+		require_once $filename;
+	}
 	
-	require_once SERVER_DIR.APP_PATH.'system/classes/YAML/sfYaml.php';
-	require_once SERVER_DIR.APP_PATH.'system/classes/Keypath.php';
-	require_once SERVER_DIR.APP_PATH.'system/classes/Settings.php';
 	
-	// Now load the remaining core classes
-	// Also, autload a few classes in the /application/classes/ directory
+	// Load in the remaining classes that offer additional functionality
+	Loader::load_class('Language');
 	Loader::load_class('Router');
 	Loader::load_class('Template');
 	Loader::load_class('QueryFactory');
@@ -31,16 +66,25 @@
 	Loader::load_class('ApplicationGenericPageController');
 	Loader::load_class('TableController');
 	Loader::load_class('Modules');
-	Loader::load_class('Cron');
+	Loader::load_class('Cron');	
 	Loader::load_class('Browser');
+	
+	
+	// Load in all the application defined functions
+	foreach(glob(APPLICATION_FUNCTIONS_DIR.'*.php') as $filename) {
+		require_once $filename;
+	}
+	
 	
 	// Conditionally include support for ActiveRecord
 	if ( version_compare(phpversion(), "5.3.0") >= 0 ) {
 		require_once CLASSES_DIR.'ActiveRecord/init.php';
 	}
 	
+	
 	// Finish initializing the system (load defines, settings, modules, set session variables, etc)
 	bootstrap_system();
+	
 	
 	// System action to allow post system-init, pre controller created actions to execute		
 	Hooks::call_hook(Hooks::HOOK_CONTROLLER_PRE_CREATED);	
