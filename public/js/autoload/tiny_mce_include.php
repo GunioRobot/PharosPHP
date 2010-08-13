861fabaeee926e0cad423bdaa1f71be405812b30
Matt Brewer <matt.brewer@me.com>
2010-08-12 23:24:47 -0400

    Moved cron.php & index.php to the application/public/ directory

--- public/js/autoload/tiny_mce_include.php
+++
@@ -1,2 +0,0 @@
-<script type="text/javascript" src="<?=PUBLIC_SERVER?>js/tinymce/jquery.tinymce.js"></script>
-<script type="text/javascript" src="<?=PUBLIC_SERVER?>js/tinymce/tiny_mce_init.js"></script>
