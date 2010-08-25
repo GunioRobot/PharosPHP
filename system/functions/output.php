b5fc4e8be15764f6f9faa396aff914d68d313906
Matt Brewer <matt.brewer@me.com>
2010-08-25 16:16:14 -0400

    Takes another param to have a selected item

--- system/functions/output.php
+++ system/functions/output.php
@@ -8,10 +8,10 @@
 	//
 	////////////////////////////////////////////////////////////////////////////////
 	
-	function array_to_dropdown($options,$id="",$name="",$style="",$class="") {
+	function array_to_dropdown($options,$default="",$id="",$name="",$style="",$class="") {
 		$html = '<select '.($id!==""?'id="'.$id.'" ':'').($name!==""?'name="'.$name.'" ':'').($style!==""?'style="'.$style.'" ':'').($class!==""?'style="'.$style.'"':'').'>';
 		foreach($options as $i => $o) {
-			$html .= '<option value="'.$i.'">'.$o.'</option>';
+			$html .= '<option value="'.$i.'" '.($default==$i?'selected="selected"':'').'>'.$o.'</option>';
 		}
 		return $html . '</select>';
 	}
