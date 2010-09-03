fd0fdaa2b9671d79e75680094cfd20df516c391a
Matt Brewer <matt.brewer@me.com>
2010-09-03 18:43:58 -0400

    Removed useless JSON module, added enqueue_script() function so that modules could more easily add javascript to the header of a page

--- system/functions/output.php
+++ system/functions/output.php
@@ -1,19 +1,44 @@
 <?
 
-	////////////////////////////////////////////////////////////////////////////////
-	//
-	//	array_to_dropdown()
-	//
-	// Returns HTML for a <select> element from the input array
-	//
-	////////////////////////////////////////////////////////////////////////////////
-	
+	/**
+	 * array_to_dropdown
+	 *
+	 * @param array $options
+	 * @param (string|String|int) $selected=""
+	 * @param (string|String) $id=""
+	 * @param (string|String) $name=""
+	 * @param (string|String) $style=""
+	 * @param (string|String) $class=""
+	 *
+	 * @return String $html
+	 * @author Matt Brewer
+	 **/
+
 	function array_to_dropdown($options,$default="",$id="",$name="",$style="",$class="") {
 		$html = '<select '.($id!==""?'id="'.$id.'" ':'').($name!==""?'name="'.$name.'" ':'').($style!==""?'style="'.$style.'" ':'').($class!==""?'style="'.$style.'"':'').'>';
 		foreach($options as $i => $o) {
 			$html .= '<option value="'.$i.'" '.($default==$i?'selected="selected"':'').'>'.$o.'</option>';
 		}
-		return $html . '</select>';
+		return new String($html.'</select>');
+	}
+	
+	
+	/**
+	 * enqueue_script
+	 *
+	 * @param (string|String|array) $path(s)
+	 *
+	 * @return void
+	 * @author Matt Brewer
+	 **/
+	function enqueue_script($scripts) {
+		if ( is_array($scripts) ) {
+			foreach($scripts as $script) {
+				Application::controller()->output->javascript($script)
+			}
+		} else {
+			Application::controller()->output->javascript(strval($scripts));
+		}
 	}
 	
 ?>
