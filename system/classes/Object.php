0e0d8b6ab248cc2092bbe9006df1c52186aa7ce8
Matt Brewer <matt.brewer@me.com>
2010-09-04 09:45:16 -0400

    Added new root class for all objects in system, appropriately named "Object"

---
+++ system/classes/Object.php
@@ -0,0 +1,30 @@
+<?
+	
+	/**
+	 * Object
+	 *
+	 * @package PharosPHP.Core.Classes
+	 * @author Matt Brewer
+	 **/
+
+	class Object {
+		
+		
+		/**
+		* Connection to the database
+		*
+		* @var Database
+		*/
+		protected $db = null;
+		
+		
+		public function __construct() {
+			
+			global $db;
+			$this->db &= $db;
+			
+		}
+		
+	}
+
+?>
