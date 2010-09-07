e0552df34ab2b97bc159e9c364f6652662991cdc
Matt Brewer <matt.brewer@me.com>
2010-09-07 13:52:09 -0400

    Call to strip_slashes() in text_area.php form_type now

--- application/modules/profile/form_types/create/text_area.php
+++ application/modules/profile/form_types/create/text_area.php
@@ -2,5 +2,5 @@
 
 $style = ($form_array['width'] != '' AND $form_array['height'] != '' ) ? 'style="width:'.$form_array['width'].';height:'.$form_array['height'].';"' : '';
 
-$item = '<textarea name="'.$form_array['name'].'" class="'.$form_array['class'].'" rows="'.$form_array['row'].'" cols="'.$form_array['col'].'" '.$style.'>'.$value.'</textarea>';
-?>
+$item = '<textarea name="'.$form_array['name'].'" class="'.$form_array['class'].'" rows="'.$form_array['row'].'" cols="'.$form_array['col'].'" '.$style.'>'.stripslashes($value).'</textarea>';
+?> 
