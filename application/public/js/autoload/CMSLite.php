d97c0dd7339b7ab7b7ee086cec6774887b358870
Matt Brewer <matt.brewer@me.com>
2010-09-14 15:39:04 -0400

    Added js function mysqlTimeStampToDate()

--- application/public/js/autoload/CMSLite.php
+++ application/public/js/autoload/CMSLite.php
@@ -21,4 +21,12 @@
 		
 	});
 	
+
+	// function parses mysql datetime string and returns javascript Date object
+	function mysqlTimeStampToDate(timestamp) {
+    	var regex=/^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) ?(?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/;
+    	var parts=timestamp.replace(regex,"$1 $2 $3 $4 $5 $6").split(' ');
+    	return new Date(parts[0],parts[1]-1,parts[2],parts[3],parts[4],parts[5]);
+  	}
+	
 </script>
