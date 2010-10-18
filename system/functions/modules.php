193912efb9bbdd5b61156bcdd58297f9e92b106d
Matt Brewer <matt.brewer@me.com>
2010-10-18 10:34:24 -0400

    Added functions for detecting where a module is installed and correctly outputting URL to that resource

---
+++ system/functions/modules.php
@@ -0,0 +1,44 @@
+<?
+
+	/**
+	 * is_shared_module
+	 * Checks to see if the module is installed in the SYSTEM_DIR directory
+	 *
+	 * @param string $path - complete path to the module
+	 *
+	 * @return boolean $status
+	 * @author Matt Brewer
+	 **/
+	function is_shared_module($path) {
+		return stripos($path, SYSTEM_PATH) !== false;
+	}
+	
+	
+	/**
+	 * module_dir
+	 * Path to the module, relative to the install root
+	 * 
+	 * @param string $path - complete path to the module
+	 *
+	 * @return string $dir
+	 * @author Matt Brewer
+	 **/
+	function module_dir($path) {
+		return (is_shared_module($path) ? basename(SYSTEM_PATH) : APP_DIR) . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . basename($path);
+	}
+	
+	
+	/**
+	 * module_url
+	 * Complete URL to the module directory
+	 *
+	 * @param string $path - complete path to the module
+	 *
+	 * @return string $url
+	 * @author Matt Brewer
+	 **/
+	function module_url($path) {
+		return ROOT_URL.module_dir($path);
+	}
+
+?>
