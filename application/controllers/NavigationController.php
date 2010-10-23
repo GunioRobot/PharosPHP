5fb8045f07ac80e48e1fcdfeadfa72b94b53e320
Matt Brewer <matt.brewer@me.com>
2010-10-22 21:02:53 -0400

    Removed unnecessary call to select_app()

--- application/controllers/NavigationController.php
+++ application/controllers/NavigationController.php
@@ -194,9 +194,7 @@
 				// Delete the navigtaion entry itself
 				$sql = "DELETE FROM ".$this->table->id." WHERE $this->dataKey = '$id' LIMIT 1";
 				$this->db->Execute($sql);
-				
-				select_app(DEFAULT_APP_ID);
-				
+								
 				$obj = array('error' => false, 'redirect' => Template::manage(__CLASS__));
 				echo json_encode((object)$obj);
 				exit;
