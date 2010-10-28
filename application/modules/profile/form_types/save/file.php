5abb9165b5a50634cd19914907a120007d20ff53
Matt Brewer <matt.brewer@me.com>
2010-10-28 12:42:43 -0400

    Removed call to "continue;" and instead set $data=null

--- application/modules/profile/form_types/save/file.php
+++ application/modules/profile/form_types/save/file.php
@@ -42,33 +42,37 @@
 		try {
 						
 			if ( $options['save_as_image'] && $options['resize_image'] && $options['image_width'] && $options['image_height'] ) { 
-				$filename = save_uploaded_file($data, array("is_image" => true, "resize" => array("width" => $options['image_width'], "height" => $options['image_height'])))
+				$filename = save_uploaded_file($data, array("is_image" => true, "resize" => array("width" => $options['image_width'], "height" => $options['image_height'])));
 			} else $filename = save_uploaded_file($data);
-			
+						
 			$_POST[$data] = $filename;
 			
 		} catch ( Exception $e ) {
 			Console::log($e->getMessage());
-			continue;
-		}
-
-		// Add the filesize to the sql statement, if it's the main video and not thumbnails, etc		
-		if ( isset($options['store_filesize']) && $options['store_filesize'] == "true" ) {
-			$size = filesize(UPLOAD_PATH.Input::post($data));
-			$sqlUpdate .= $data.'_file_size = "'.$size.'", ';
-			$sqlFields .= $data.'_file_size,';
-			$sqlValues .= '"'.$size.'",';
+			$data = null;
 		}
 		
-		// Possible store the file type
-		if ( isset($options['store_file_type']) && $options['store_file_type'] == "true" ) {
+		if ( $data ) {
 
-			$info = pathinfo(UPLOAD_PATH.Input::post($data));
-			$ext = $info['extension'];
-			if ( $ext != "" ) {
-				$sqlUpdate .= $data.'_file_type = "'.$ext.'", ';
-				$sqlFields .= $data.'_file_type,';
-				$sqlValues .= '"'.$ext.'",';
+			// Add the filesize to the sql statement, if it's the main video and not thumbnails, etc		
+			if ( isset($options['store_filesize']) && $options['store_filesize'] == "true" ) {
+				$size = filesize(UPLOAD_PATH.Input::post($data));
+				$sqlUpdate .= $data.'_file_size = "'.$size.'", ';
+				$sqlFields .= $data.'_file_size,';
+				$sqlValues .= '"'.$size.'",';
+			}
+		
+			// Possible store the file type
+			if ( isset($options['store_file_type']) && $options['store_file_type'] == "true" ) {
+
+				$info = pathinfo(UPLOAD_PATH.Input::post($data));
+				$ext = $info['extension'];
+				if ( $ext != "" ) {
+					$sqlUpdate .= $data.'_file_type = "'.$ext.'", ';
+					$sqlFields .= $data.'_file_type,';
+					$sqlValues .= '"'.$ext.'",';
+				}
+			
 			}
 			
 		}
