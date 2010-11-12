f14ad479c34094229808696a15b9a578f347c0c8
Matthew Brewer <matt@Matthews-MacBook.local>
2010-11-12 11:49:30 -0500

    Made the $message parameter to PharosBaseException constructor optional

--- system/classes/Exceptions.php
+++ system/classes/Exceptions.php
@@ -9,7 +9,7 @@
 	 **/
 	
 	class PharosBaseException extends Exception {
-		public function __construct($message) {
+		public function __construct($message="") {
 			parent::__construct();
 			$this->message = $message;
 		}
