49f218fc0602fa348e47ad18e40ce79e4f251fcf
Matthew Brewer <matt@Matthews-MacBook.local>
2010-11-29 08:03:26 -0500

    Updated to work with the submodule, intelligentely connects to database based upon application environmnet settings.

--- system/classes/ActiveRecord/init.php
+++ system/classes/ActiveRecord/init.php
@@ -1,11 +1,10 @@
 <?
 	
-	require_once dirname(__FILE__).'/ActiveRecord.php';
-	
-	ActiveRecord\Config::initialize(function($cfg)
-	{
+	require_once dirname(__FILE__) . DS . 'module' . DS . 'ActiveRecord.php';
+	ActiveRecord\Config::initialize(function($cfg) {
 	    $cfg->set_model_directory(MODELS_PATH);
-	    $cfg->set_connections(array('development' => sprintf('mysql://%s:%s@%s/%s', Settings::get('application.database.username'), Settings::get('application.database.password'), Settings::get('application.database.host'), Settings::get('application.database.name'))));
+		$environment = Application::environment();
+	    $cfg->set_connections(array($environment->env => sprintf('mysql://%s:%s@%s/%s', $environment->settings->database->username, $environment->settings->database->password, $environment->settings->database->host, $environment->settings->database->name)));
 	});
 
 ?>
