acdbefce5aa882720cbd26afffe3d528d6e9d998
Matthew Brewer <matt@Matthew-Brewers-iMac.local>
2011-03-27 18:29:44 -0400

    Fixed stylesheet path for exception & error views

--- application/views/errors/exception.php
+++ application/views/errors/exception.php
@@ -5,7 +5,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>Pharos PHP | Uncaught Exception</title>
-	<link rel="stylesheet" type="text/css" href="<?=ROOT_URL . APP_DIR . DS . PUBLIC_DIR . DS . 'css' . DS?>styles.css" />
+	<link rel="stylesheet" type="text/css" href="<?=ROOT_URL . APP_DIR . DS . PUBLIC_DIR . DS . 'css' . DS?>style_site.css" />
 </head>
 
 <body>
