86a8333109778b752320f3ee247bf51ef12a649e
Matthew Brewer <matt@Matthew-Brewers-iMac.local>
2011-04-24 19:47:02 -0400

    Did not like having GenericPagesController in system/ inheriting from ApplicationController in application/, so just removed the class & have methods defined in ApplicationGenericPageController itself

--- application/classes/ApplicationGenericPageController.php
+++ application/classes/ApplicationGenericPageController.php
@@ -8,8 +8,72 @@
 	 * @author Matt Brewer
 	 **/
 	
-	require_once CLASSES_PATH.'GenericPageController.php';
-	class ApplicationGenericPageController extends GenericPageController {
+	class ApplicationGenericPageController extends ApplicationController {
+		
+		protected $text = "";
+		protected $page = null;
+
+		public function __construct($title="") {
+			parent::__construct();
+			$this->title = $title;
+		}
+
+		public function headerView() {
+			return '';
+		}
+
+		protected function footerView() {
+			return '';
+		}
+
+
+		public function index() {
+			echo $this->text();
+		}
+
+		public function page($page=null) {
+			if ( !is_null($page) ) {
+
+				$this->page = $page;
+
+				$this->text($page->text);
+			}
+		}
+
+		public function text($text="") {
+			if ( $text != "" ) { 
+				$this->text = $text;
+			} else return $this->text;
+		}
+
+
+		public function output($string="") {			
+			if ( $string != '' ) {
+				$this->output = $string;
+			} else {
+
+				if ( $this->output === "" ) {
+
+					return "";
+
+				} else {
+
+					$s = "";
+					if ( method_exists($this, "headerView") ) {
+						$s .= $this->headerView();
+					}
+
+					$s .= $this->output;
+
+					if ( method_exists($this, "footerView") ) {
+						$s .= $this->footerView();
+					}
+					return $s;
+
+				}
+
+			}
+		}
 		
 	}
 
