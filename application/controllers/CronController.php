429f0ddd05fd4195eebc6069d2ee203bf44e8062
Matthew Brewer <matt@Matthew-Brewers-iMac.local>
2011-04-24 20:01:50 -0400

    Additional Documentation

--- application/controllers/CronController.php
+++ application/controllers/CronController.php
@@ -9,11 +9,25 @@
 	
 	class CronController extends ApplicationController {
 		
+		/**
+		 * Controller Initializer
+		 *
+		 * @return void
+		 * @author Matt Brewer
+		 **/
+		
 		public function __construct() {
 			parent::__construct();
 			$this->auth->login_required(false);
 		}
 		
+		/**
+		 * Web handler for default method
+		 *
+		 * @return void
+		 * @author Matt Brewer
+		 **/
+
 		public function index() {
 			Cron::process();
 		}
